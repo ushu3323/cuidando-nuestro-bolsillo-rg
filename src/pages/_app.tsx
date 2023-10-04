d61f6e1b8d94d87e64036c1aa118fb09a127c967
ushu3323 <redspower10@gmail.com>
2023-10-03 22:26:35 -0300

    Instalar primereact

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import { type AppType } from "next/app";
-
+import { PrimeReactProvider } from "primereact/api";
 import { api } from "~/utils/api";
 
+import Tailwind from "primereact/passthrough/tailwind";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <PrimeReactProvider value={{ unstyled: true, pt: Tailwind }}>
+      <Component {...pageProps} />
+    </PrimeReactProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,11 @@
 import { type Config } from "tailwindcss";
 
 export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/primereact/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
