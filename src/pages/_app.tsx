4c4db8efd2ecbd92b84afe64c8263726e6e5b623
ushu3323 <redspower10@gmail.com>
2023-11-16 13:14:23 -0300

    Mover Head a archivo inicial

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,20 @@ import { api } from "~/utils/api";
 import "primeicons/primeicons.css";
 import "primereact/resources/themes/lara-light-blue/theme.css";
 
+import Head from "next/head";
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <PrimeReactProvider value={{ ripple: true }}>
+      <Head>
+        <title>Cuidando Nuestro Bolsillo</title>
+        <meta
+          name="description"
+          content="Colaboramos entre todos para ahorrar en Rio Grande!"
+        />
+        <link rel="icon" href="/favicon.svg" />
+      </Head>
       <Component {...pageProps} />
     </PrimeReactProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,9 @@
-import Head from "next/head";
 import ProductsList from "../components/ProductsList";
 import Header from "../components/layout/Header";
 
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Cuidando Nuestro Bolsillo</title>
-        <meta
-          name="description"
-          content="Colaboramos entre todos para ahorrar en Rio Grande!"
-        />
-        <link rel="icon" href="/favicon.svg" />
-      </Head>
       <main className="relative flex min-h-[100dvh] max-w-none flex-col items-stretch">
         <Header />
         <ProductsList />
