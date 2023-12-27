139425835f7aefd606dcfbc3f4ef687bdfbdf1c2
ushu3323 <redspower10@gmail.com>
2023-12-27 11:57:27 -0300

    Usar CircularProgress de mui en pagina de carga

--- src/components/LoadingPage.tsx
+++ src/components/LoadingPage.tsx
@@ -1,10 +1,10 @@
-import { ProgressSpinner } from "primereact/progressspinner";
+import { CircularProgress } from "@mui/material";
 import { type ReactNode } from "react";
 
 export default function LoadingPage({ children }: { children?: ReactNode }) {
   return (
     <div className="fixed left-0 top-0 flex h-full w-full flex-col items-center justify-center">
-      <ProgressSpinner />
+      <CircularProgress />
       {children}
     </div>
   );
