d93f24a70e7a21c8458f44337d0c5135d7eb1f05
ushu3323 <redspower10@gmail.com>
2023-12-27 12:12:08 -0300

    Usar componentes de MUI Material en Header

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,50 +1,27 @@
-import { signOut, useSession } from "next-auth/react";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import { PrimeIcons } from "primereact/api";
-import { Button } from "primereact/button";
-import { type MenuItem } from "primereact/menuitem";
-import { ProgressSpinner } from "primereact/progressspinner";
+import { AppBar, CircularProgress, Toolbar, Typography } from "@mui/material";
+import { useSession } from "next-auth/react";
 import AvatarMenu from "./AvatarMenu";
 
 export default function Header() {
   const { data, status } = useSession();
   const user = data?.user;
-  const router = useRouter();
-
-  const menuItems: MenuItem[] = [
-    {
-      label: "Cerrar Sesion",
-      icon: PrimeIcons.TIMES,
-      command: () => void signOut(),
-    },
-  ];
-
-  const Actions = () =>
-    user ? (
-      <div className="flex items-center gap-5">
-        <AvatarMenu user={user} menuItems={menuItems} />
-      </div>
-    ) : (
-      <>
-        <Button
-          label="Acceder"
-          severity="help"
-          onClick={() => void router.push("/auth/login")}
-        />
-      </>
-    );
 
   return (
-    <header className="sticky top-0 z-40 flex h-14 items-center justify-between bg-white px-4 py-2 shadow">
-      <Link href="/" className="text-lg font-bold text-black no-underline">
-        Precios RG
-      </Link>
-      {status === "loading" ? (
-        <ProgressSpinner style={{ margin: 0, height: "100%" }} />
-      ) : (
-        <Actions />
-      )}
-    </header>
+    <AppBar position="sticky">
+      <Toolbar>
+        <Typography variant="h5" fontWeight={700} sx={{ flexGrow: 1 }}>
+          Precios RG
+        </Typography>
+        {status === "loading" ? (
+          <CircularProgress></CircularProgress>
+        ) : (
+          user && (
+            <div className="flex items-center gap-5">
+              <AvatarMenu user={user} />
+            </div>
+          )
+        )}
+      </Toolbar>
+    </AppBar>
   );
 }
