fd071fb20738c95978340fcf4cbee4fca6d5e424
ushu3323 <redspower10@gmail.com>
2023-12-28 02:42:03 -0300

    Crear modelo de Publicacion (Post)

---
+++ prisma/migrations/20231228053831_create_post_model/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" TEXT NOT NULL DEFAULT gen_random_uuid(),
+    "productId" TEXT NOT NULL,
+    "commerceId" TEXT NOT NULL,
+    "price" DECIMAL(20,2) NOT NULL,
+    "authorUID" TEXT NOT NULL,
+    "publishDate" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_commerceId_fkey" FOREIGN KEY ("commerceId") REFERENCES "Commerce"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
