b57dee965ab164e660ad7cadc18547955e4e2506
ushu3323 <redspower10@gmail.com>
2023-12-29 12:07:42 -0300

    Hacer layout global y usar FAB en base a configuracion de pagina

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,21 +1,39 @@
-import { Container, ContainerProps } from "@mui/material";
-import { type PropsWithChildren } from "react";
+import {
+  Container,
+  Slide,
+  useScrollTrigger,
+  type ContainerProps,
+} from "@mui/material";
+import type { PropsWithChildren } from "react";
+import React from "react";
 import Header from "./Header/Header";
 
-interface LayoutProps {
+export interface LayoutProps {
   containerProps?: ContainerProps;
+  /** @default true */
+  showHeader?: boolean;
+  fab?: () => React.JSX.Element;
 }
 
 export default function Layout({
   children,
   containerProps,
+  showHeader = true,
+  fab,
 }: PropsWithChildren<LayoutProps>) {
+  const trigger = useScrollTrigger();
+
   return (
     <>
-      <Header />
-      <Container maxWidth="sm" {...containerProps}>
+      {showHeader && <Header />}
+      <Container className="relative" maxWidth="sm" {...containerProps}>
         {children}
       </Container>
+      {fab && (
+        <Slide appear={false} direction="up" in={!trigger}>
+          {fab()}
+        </Slide>
+      )}
     </>
   );
 }
