9a53b7971a4e39a477d7bb3fb4140a0270938ac8
ushu3323 <redspower10@gmail.com>
2023-12-31 19:19:22 -0300

    Subir imagen al crear una publicacion

--- src/pages/posts/new.tsx
+++ src/pages/posts/new.tsx
@@ -1,5 +1,5 @@
 import { Box, Snackbar } from "@mui/material";
-import { type TRPCClientError } from "@trpc/client";
+import { TRPCClientError } from "@trpc/client";
 import { type FormikHelpers } from "formik";
 import type { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
@@ -14,42 +14,64 @@ import { api } from "~/utils/api";
 
 export default function NewPostPage() {
   const router = useRouter();
+  const trpc = api.useContext();
   const [showSnackbar, setShowSnackbar] = useState(false);
   const [snackbarMsg, setSnackbarMsg] = useState("");
   const { mutateAsync } = api.post.create.useMutation({
     cacheTime: 0,
   });
 
-  const handleOnSubmit = (
+  const handleOnSubmit = async (
     values: NewPostFormFields,
     { setErrors }: FormikHelpers<NewPostFormFields>,
-  ) =>
-    mutateAsync({
-      productId: values.productId,
-      commerceId: values.commerceId,
-      price: values.price,
-      image: "",
-    })
-      .then((offer) => {
-        console.log(offer);
-        void router.push("/");
-      })
-      .catch((err: TRPCClientError<AppRouter>) => {
+  ) => {
+    try {
+      const image = values.image;
+      const signedUrlPayload = await trpc.post.getImageSignedUrl.fetch({
+        length: image.size,
+        mimetype: image.type,
+      });
+
+      // Submit image
+      await fetch(signedUrlPayload.url, {
+        method: "PUT",
+        body: image,
+        headers: {
+          "Content-Type": image.type,
+        },
+      });
+
+      await mutateAsync({
+        productId: values.productId,
+        commerceId: values.commerceId,
+        price: values.price,
+        imageKey: signedUrlPayload.key,
+      });
+      void router.push("/");
+    } catch (error) {
+      if (error instanceof TRPCClientError) {
+        const err = error as TRPCClientError<AppRouter>;
         const fieldErrors = err?.data?.zodError?.fieldErrors;
         if (fieldErrors) {
-          console.log("Catch field errors on mutation", fieldErrors);
-          void setErrors({
-            productId: fieldErrors.productId?.join("\n"),
-            commerceId: fieldErrors.commerceId?.join("\n"),
-            price: fieldErrors.price?.join("\n"),
-          });
+          console.log("Catch field errors", fieldErrors);
+          if (err.shape?.data.path === "post.create") {
+            void setErrors({
+              productId: fieldErrors.productId?.join("\n"),
+              commerceId: fieldErrors.commerceId?.join("\n"),
+              price: fieldErrors.price?.join("\n"),
+              image: fieldErrors.image?.join("\n"),
+            });
+          }
           return;
         }
-        setShowSnackbar(true);
-        setSnackbarMsg(
-          "Hubo un error al intentar publicar, intente de nuevo en unos minutos",
-        );
-      });
+      }
+      console.error(error);
+    }
+    setSnackbarMsg(
+      "Hubo un error inesperado mientras se publicaba, intentelo de nuevo mas tarde",
+    );
+    setShowSnackbar(true);
+  };
 
   const handleSnackbarClose = () => setShowSnackbar(false);
 
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,12 +1,96 @@
+import {
+  CopyObjectCommand,
+  DeleteObjectCommand,
+  HeadObjectCommand,
+  PutObjectCommand,
+} from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { Prisma } from "@prisma/client";
+import { TRPCError } from "@trpc/server";
+import { randomUUID } from "crypto";
 import { z } from "zod";
+import { env } from "~/env.mjs";
 import {
   createTRPCRouter,
   protectedProcedure,
   publicProcedure,
 } from "~/server/api/trpc";
+import { s3 } from "~/utils/s3";
+
+const IMAGE_PREFIX = "pre-";
+
+/**
+ * Removes S3 image's object from "object deletion lifecycle" by removing the "pre-" prefix
+ * and returns its public url
+ *
+ * @param imageKeyWithoutPrefix
+ * @returns {string} Image public url
+ */
+
+const validateS3image = async (imageKeyWithoutPrefix: string) => {
+  await s3
+    .send(
+      new HeadObjectCommand({
+        Bucket: env.S3_BUCKET_NAME,
+        Key: IMAGE_PREFIX + imageKeyWithoutPrefix,
+      }),
+    )
+    .catch(() => {
+      throw new TRPCError({
+        code: "BAD_REQUEST",
+        message: "IMAGE_KEY_NOT_FOUND",
+      });
+    });
+
+  const copyOutput = await s3.send(
+    new CopyObjectCommand({
+      CopySource:
+        `${env.S3_BUCKET_NAME}/` + `${IMAGE_PREFIX}${imageKeyWithoutPrefix}`,
+      Bucket: env.S3_BUCKET_NAME,
+      Key: imageKeyWithoutPrefix,
+    }),
+  );
+
+  if (!copyOutput.CopyObjectResult) {
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "IMAGE_VALIDATION_ERROR",
+    });
+  }
+
+  await s3.send(
+    new DeleteObjectCommand({
+      Bucket: env.S3_BUCKET_NAME,
+      Key: IMAGE_PREFIX + imageKeyWithoutPrefix,
+    }),
+  );
+
+  return `${env.S3_BUCKET_URL}/${imageKeyWithoutPrefix}`;
+};
 
 export const postsRouter = createTRPCRouter({
+  getImageSignedUrl: protectedProcedure
+    .input(z.object({ mimetype: z.string(), length: z.number() }))
+    .query(async ({ input, ctx }) => {
+      const key = `${randomUUID()}`;
+      const expiresIn = 3600;
+
+      const url = await getSignedUrl(
+        s3,
+        new PutObjectCommand({
+          Bucket: env.S3_BUCKET_NAME,
+          Key: IMAGE_PREFIX + key,
+          ContentType: input.mimetype,
+          ContentLength: input.length,
+          Metadata: {
+            author: ctx.session.user.id,
+          },
+        }),
+        { expiresIn },
+      );
+
+      return { url, key, expiresIn };
+    }),
   create: protectedProcedure
     .input(
       z.object({
@@ -18,16 +102,14 @@ export const postsRouter = createTRPCRouter({
           .string()
           .nonempty({ message: "El comercio no debe quedar vacio" })
           .pipe(z.string().uuid()),
-        image: z
-          .string()
-          .nonempty({ message: "La imagen es requerida" })
-          .url({ message: "Url invalido" }),
+        imageKey: z.string().nonempty({ message: "La imagen es requerida" }),
         price: z
           .number({ invalid_type_error: "Debe ingresar el precio" })
           .positive({ message: "Ingrese un precio valido" }),
       }),
     )
     .mutation(async ({ ctx, input }) => {
+      const imageUrl = await validateS3image(input.imageKey);
       return ctx.db.post.create({
         data: {
           product: {
@@ -38,7 +120,7 @@ export const postsRouter = createTRPCRouter({
               id: input.commerceId,
             },
           },
-          image: input.image,
+          image: imageUrl,
           price: new Prisma.Decimal(input.price),
           author: {
             connect: {
